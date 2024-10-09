f4375c9d0cad2bf1c3b0587b563d94aa11e8c679
Boris <mssync99999@gmail.com>
2024-10-09 19:20:38 +0300

    09-10-2024 1920

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+insert into mpas (name) values ('G');
+insert into mpas (name) values ('PG');
+insert into mpas (name) values ('PG-13');
+insert into mpas (name) values ('R');
+insert into mpas (name) values ('NC-17');
+
+insert into genres (name) values ('Комедия');
+insert into genres (name) values ('Драма');
+insert into genres (name) values ('Мультфильм');
+insert into genres (name) values ('Триллер');
+insert into genres (name) values ('Документальный');
+insert into genres (name) values ('Боевик');
